7a523ae427fa5d4ac2eec562b097fbca86a3c275
Avaneesh U G K <avaneeshk-v@tataprojects.com>
2024-02-27 11:35:41 +0000

    dynamic srv push

--- gen/srv/srv/cat-service.js
+++ gen/srv/srv/cat-service.js
@@ -52,30 +52,32 @@ module.exports = cds.service.impl(async function(){
         
     // })
 
-    this.on('postUserDataDate',async (req)=>{
-        console.log(req?.data);
-        let dataSend = await cds.connect.to("UserDataDate");
+    // this.on('postUserDataDate',async (req)=>{
+    //     console.log(req?.data);
+    //     let dataSend = await cds.connect.to("UserDataDate");
         
 
-        try{
-            if(req?.data != undefined){
-                var uri = "/postUserDataDate(userName='"+ req.data.userName + "',fromDate='" + req.data.fromDate + "',toDate='"+ req.data.toDate + "')";
-                // let user_dat = await dataSend.get(uri);
-            }
-            let url = "https://tata-projects-limited-btp-dev-0or0hi20-dev-space-pandemo-srv.cfapps.eu10-004.hana.ondemand.com/odata/v4/catalog" + uri;
-            console.log(url);
-            const res = await axios.get(url,{
-                timeout:600000
-            });
-            console.log(res);
-        } catch(err) {
-            console.log(err);
-            throw err
-        }
-        return "Username and Dates succesfully transferred";      
-    })
+    //     try{
+    //         if(req?.data != undefined){
+    //             var uri = "/postUserDataDate(userName='"+ req.data.userName + "',fromDate='" + req.data.fromDate + "',toDate='"+ req.data.toDate + "')";
+    //             // let user_dat = await dataSend.get(uri);
+    //         }
+    //         let url = "https://tata-projects-limited-btp-dev-0or0hi20-dev-space-pandemo-srv.cfapps.eu10-004.hana.ondemand.com/odata/v4/catalog" + uri;
+    //         console.log(url);
+    //         const res = await axios.get(url,{
+    //             timeout:600000
+    //         });
+    //         console.log(res);
+    //     } catch(err) {
+    //         console.log(err);
+    //         throw err
+    //     }
+    //     return "Username and Dates succesfully transferred";      
+    // })
 
     this.on('postProjectID',async (req)=>{
+
+
         
 
         if(req?.data != undefined){
@@ -83,9 +85,9 @@ module.exports = cds.service.impl(async function(){
                 
                 var auth = req?.headers?.authorization;
                 if (auth != undefined){
-                    // console.log(auth);
+
                     var token = auth.split(" ");
-                    // console.log(token[1]);
+
                     var decoded = decode.jwtDecode(token[1]);
                     console.log(decoded);
                     console.log(decoded["user_name"]);
@@ -100,7 +102,60 @@ module.exports = cds.service.impl(async function(){
                     }
                     
                     var uri = "/postUserDataDate(userName='"+ userpassed + "',projectid='" + req?.data.ID + "')";
-                    let url = "https://tata-projects-limited-tpl-ariba-uat-dzu885iy-tpl-aruat-3e0f9ade.cfapps.eu10-004.hana.ondemand.com/odata/v4/pan-approval" + uri;
+
+
+        //////////////////////////////////////Dynamic srv url get////////////////////////////////////////////
+
+        var vcap = JSON.parse(process.env.VCAP_SERVICES);
+        var panformDest;
+        let auth = req?.headers?.authorization;
+        console.log(auth);
+        vcap.destination.forEach((dest)=>{
+            if (dest?.name != undefined && dest?.name == "PAN_Form_Data-destination-service"){
+                panformDest = dest;
+            }
+        })
+        // panformDest  = vcap.destination[0];
+
+
+        var tokenurl = panformDest.credentials.url + "/oauth/token?grant_type=client_credentials";
+        var basicAuth = panformDest.credentials.clientid + ":" + panformDest.credentials.clientsecret;
+        var basicAuth = btoa(basicAuth);
+        var basicStr = "Basic " + basicAuth;
+         
+
+        var axiosTokenResp = await axios.request({
+            url: tokenurl,
+            method: 'get',
+            headers:{
+                Authorization : basicStr
+            }
+        })
+        var accesstoken = axiosTokenResp.data.access_token;
+        
+        var authDest = axiosTokenResp.data.token_type + " " + accesstoken;
+        console.log(authDest);
+
+        var destinationurl = panformDest.credentials.uri + "/destination-configuration/v1/destinations/PAN_Form_Data-srv-api"
+
+        var destinationResp = await axios.request({
+            url: destinationurl,
+            method: 'get',
+            headers:{
+                Authorization : authDest
+            }
+        })
+
+        var baseSrvUrl = destinationResp?.data?.destinationConfiguration?.URL;
+
+
+
+        ///////////////////////////////////////////////////////////////////////////////////
+
+                    
+                    // let url = "https://tata-projects-limited-tpl-ariba-uat-dzu885iy-tpl-aruat-3e0f9ade.cfapps.eu10-004.hana.ondemand.com/odata/v4/pan-approval" + uri;
+                    // let url = "https://tata-projects-limited-btp-dev-0or0hi20-dev-space-pan-fo3df293e9.cfapps.eu10-004.hana.ondemand.com/odata/v4/pan-approval" + uri;
+                    let url = baseSrvUrl + "/odata/v4/pan-approval" + uri;
                     var res = await axios.get(url,{
                         timeout:180000
                     });
